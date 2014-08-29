1d1f628192f0da637244c4dee8907aa572ce3d27
Paul Oguda <mcpaul2058@gmail.com>
2014-08-29 04:05:19 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ksmourplace::Application.config.secret_key_base = 'cf5f8503e79c863227a58e1adff35ca3239a3f2f209cadb4240877ec86861d8203fb77e0055eb65414c04340d76dbd7ac8591f83c98913657ed13b2f3b2c9701'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ksmourplace::Application.config.session_store :cookie_store, key: '_ksmourplace_session'
